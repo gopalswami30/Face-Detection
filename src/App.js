bf3091becf45712c0fd4b5f4f541b7c1ef3f9c9e
gopalswami30 <gopalswami538@gmail.com>
2021-05-29 09:51:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Components/Navigation/Navigation'
+import Logo from './Components/Logo/Logo'
+import 'tachyons'
+function App() {
+  return (
+   <div> 
+  <Navigation></Navigation>
+  <Logo></Logo>
+  </div>
+  );
+}
+
+export default App;
